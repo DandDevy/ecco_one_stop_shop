6730b72d612767333c0f0ed752ac3cf3d78a7c48
Daniel Ashcroft <dashcroft4@gmail.com>
2019-06-20 12:11:22 +0100

    home page done. now going to start doing the learn page which will link and content stuff for the user to learn and discover

--- routes/learn.js
+++ routes/learn.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET learn page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('learn');
 });
 
 module.exports = router;
